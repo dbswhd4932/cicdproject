b80c8f414a79dc6bf4a61f097c1d9fbf2f22c027
yoonjong <dbswhd4932@gmail.com>
2023-01-05 10:29:52 +0900

    커밋

--- src/main/java/com/example/cicdproject/CicdprojectApplication.java
+++ src/main/java/com/example/cicdproject/CicdprojectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CicdprojectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CicdprojectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CicdprojectApplication.class, args);
+	}
 
 }

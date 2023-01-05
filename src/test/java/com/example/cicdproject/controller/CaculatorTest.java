9dcd13c13500f02e60245e1490621741344bc26f
yoonjong <dbswhd4932@gmail.com>
2023-01-06 00:47:39 +0900

    test 코드

--- src/test/java/com/example/cicdproject/controller/CaculatorTest.java
+++ src/test/java/com/example/cicdproject/controller/CaculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CaculatorTest {
     @Test
     void plus() {
-        int result = Calculator.plus(1, 2); // 에러
-        assertEquals(5, result);
+        int result = Calculator.plus(1, 2);
+        assertEquals(3, result);
     }
 }
